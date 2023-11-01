c5b8beaa86cc2d5acb1cee1de885ff45b81c3341
Mark Horn <markhorn.dev@gmail.com>
2023-11-01 15:51:31 -0400

    again and again

--- src/routes/test/+page.server.ts
+++ src/routes/test/+page.server.ts
@@ -3,8 +3,9 @@ import path from 'path';
 
 export async function load() {
     const file = path.join(process.cwd(), 'posts', 'test.json');
-    const data = fs.readFile(file, 'utf8');
-    console.log(file)
+    const data = await fs.readFile(file, 'utf8');
+    const json = JSON.parse(data);
+    console.log(json)
     return {
       props: {
         data,
