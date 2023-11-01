dfa9d826d689ce7424b4e219405015d89a385fe9
Mark Horn <markhorn.dev@gmail.com>
2023-11-01 18:23:17 -0400

    import meta glob

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -1,9 +1,13 @@
 <script>
-
+    const posts = Object.entries(import.meta.glob('/.data/**/*.md', { eager: true }))
+    console.log(posts)
 </script>
 
 <div class="container">
     <h1>POSTS</h1>
+    {#each posts as post}
+        {post[0]}<br/>
+    {/each}
 </div>
 
 
