2250aa4ac6fb10728b2ae88aecdb67d6c9ac7144
Mark Horn <markhorn.dev@gmail.com>
2023-11-01 16:55:00 -0400

    mdsvex test vercel

---
+++ src/routes/posts/+page.md
@@ -0,0 +1,30 @@
+
+
+<div class="container">
+
+## HELLO README
+
+```js
+function hello() {
+    console.log('hello');
+}
+function hello() {
+    console.log('hello');
+}
+function hello() {
+    console.log('hello');
+}
+```
+
+</div>
+
+<style>
+    .container {
+        flex: 1;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        padding-top: 8rem;
+        padding-bottom: 8rem;
+    }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
 
@@ -8,9 +8,7 @@ const config = {
 	extensions: ['.svelte', '.svx', '.md'],
 	preprocess: [
 		vitePreprocess(),
-		mdsvex({
-			extensions: ['.md', '.svx']
-		})
+		mdsvex({extensions: ['.md', '.svx']})
 	],
 	kit: {
 		adapter: adapter()
