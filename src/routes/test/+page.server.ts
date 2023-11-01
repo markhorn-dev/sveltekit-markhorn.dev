4972c54faf2503ec5deeae121b072354da8bda54
Mark Horn <markhorn.dev@gmail.com>
2023-11-01 14:30:11 -0400

    try again

--- src/routes/test/+page.server.ts
+++ src/routes/test/+page.server.ts
@@ -2,11 +2,16 @@ import fs from 'fs/promises';
 import path from 'path';
 
 export async function load() {
-    const file = path.resolve('static', 'testProject.json');
+    //nope
+    //const file = path.resolve('static', 'testProject.json');
+    
+    const file = path.join(process.cwd(), 'static', 'testProject.json');
+
     console.log(file);
-    const json = await fs.readFile(file,"utf8");
+    const json = await fs.readFile(file, 'utf8');
     const parsed = JSON.parse(json);
     console.log(parsed);
+    
     return {
         parsed
     }
