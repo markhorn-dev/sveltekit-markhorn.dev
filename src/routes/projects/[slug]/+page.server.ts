e8f95f77cd68d876416e7ddcbadc94aaac7a4c4e
Mark Horn <markhorn.dev@gmail.com>
2023-11-01 13:35:43 -0400

    try fix file paths in projects/[slug] (server)

--- src/routes/projects/[slug]/+page.server.ts
+++ src/routes/projects/[slug]/+page.server.ts
@@ -9,13 +9,20 @@ export async function load({ params }) {
 
     try {    
         //load the json data
-        const jsonFile = path.join(process.cwd(), '.data/projects', params.slug, 'project.json');
-        const jsonData = await fs.readFile(jsonFile, 'utf-8');
-        const parsedJson = JSON.parse(jsonData);
+        const testFile = path.resolve(process.cwd(),'.data/projects',params.slug, 'project.json');
+        console.log(testFile);
+        const testJson = await fs.readFile(testFile,"utf8");
+        const parsedJson = JSON.parse(testJson);
+        console.log(testJson);
+        // const jsonFile = '/.data/projects/'+ params.slug + 'project.json' + 'utf8';
+        // //const jsonFile = path.join(process.cwd(), '.data/projects', params.slug, 'project.json');
+        // const jsonData = await fs.readFile(process.cwd() + jsonFile, 'utf-8');
+        
 
         //load the markdown data
-        const markdownFile = path.join(process.cwd(), '.data/projects', params.slug, 'project.md');
-        const markdownData = await compileMarkdown(markdownFile);
+        //const markdownFile = path.join(process.cwd(), '.data/projects', params.slug, 'project.md');
+        //const markdownData = await compileMarkdown(markdownFile);
+        const markdownData = '';
         
         //assemble the article
         const project: Project = parsedJson;
